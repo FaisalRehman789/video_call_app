8f4feb57ab318a91b12687dcae5312235cff3c56
FaisalRahman789 <fa.faisalrahman016@gmail.com>
2024-07-09 13:10:28 +0530

    final commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'localhost:300'
+      },
+    ],
+  },
+};
 
 export default nextConfig;
